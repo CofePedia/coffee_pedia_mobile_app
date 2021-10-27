28e0ec9f6d54ef92be192f28262dd538b5704bc1
MahmoudElbokl <mds123456780>
2021-10-27 10:19:10 +0200

    Tabs.

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -6,11 +6,11 @@ class Assets {
   static const String iconsCoffePediaLogo = 'assets/icons/CoffePediaLogo.png';
   static const String iconsFilter = 'assets/icons/filter.svg';
   static const String iconsHeart = 'assets/icons/heart.svg';
-  static const String iconsHome = 'assets/icons/home.svg';
-  static const String iconsMore = 'assets/icons/More.svg';
-  static const String iconsProfile = 'assets/icons/Profile.svg';
+  static const String iconsHome = 'assets/icons/home.png';
+  static const String iconsMore = 'assets/icons/More.png';
+  static const String iconsProfile = 'assets/icons/Profile.png';
   static const String iconsSearch = 'assets/icons/Search.svg';
-  static const String iconsShoppingBasket = 'assets/icons/ShoppingBasket.svg';
+  static const String iconsShoppingBasket = 'assets/icons/ShoppingBasket.png';
   static const String iconsStaractive = 'assets/icons/Staractive.svg';
   static const String imagesJar = 'assets/images/Jar.png';
   static const String imagesPack = 'assets/images/Pack.png';
@@ -20,5 +20,4 @@ class Assets {
   static const String imagesSplash = 'assets/images/splash.png';
   static const String translationsAr = 'assets/translations/ar.json';
   static const String translationsEn = 'assets/translations/en.json';
-
 }
