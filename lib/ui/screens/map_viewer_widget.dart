76c5f66f149cdcd4c6766405574668c5556ba92a
amrazzam0 <amr.azzam@waffarha.com>
2021-12-01 12:58:52 +0200

    update

--- lib/ui/screens/map_viewer_widget.dart
+++ lib/ui/screens/map_viewer_widget.dart
@@ -63,129 +63,132 @@ class _MapViewerWidgetState extends State<MapViewerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200.h,
-      width: 343.w,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(71, 73, 73, 0),
-            Color(0xff474949),
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12.r),
+      child: Container(
+        height: 200.h,
+        width: 343.w,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(71, 73, 73, 0),
+              Color(0xff474949),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12.r),
         ),
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12.r),
-      ),
-      child: MapLayoutBuilder(
-        controller: controller,
-        builder: (context, transformer) {
-          final centerLocation = Offset(
-            transformer.constraints.biggest.width / 2,
-            transformer.constraints.biggest.height / 2,
-          );
-          final centerMarkerWidget = _buildMarkerWidget(
-            centerLocation,
-            Color(0xffFFA183),
-          );
-          return GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onDoubleTap: _onDoubleTap,
-            onScaleStart: _onScaleStart,
-            onScaleUpdate: _onScaleUpdate,
-            onTapUp: (details) {
-              final location =
-                  transformer.fromXYCoordsToLatLng(details.localPosition);
-
-              final clicked = transformer.fromLatLngToXYCoords(location);
-
-              debugPrint('${location.longitude}, ${location.latitude}');
-              debugPrint('${clicked.dx}, ${clicked.dy}');
-              debugPrint(
-                  '${details.localPosition.dx}, ${details.localPosition.dy}');
-            },
-            child: Listener(
+        child: MapLayoutBuilder(
+          controller: controller,
+          builder: (context, transformer) {
+            final centerLocation = Offset(
+              transformer.constraints.biggest.width / 2,
+              transformer.constraints.biggest.height / 2,
+            );
+            final centerMarkerWidget = _buildMarkerWidget(
+              centerLocation,
+              Color(0xffFFA183),
+            );
+            return GestureDetector(
               behavior: HitTestBehavior.opaque,
-              onPointerSignal: (event) {
-                if (event is PointerScrollEvent) {
-                  final delta = event.scrollDelta;
+              onDoubleTap: _onDoubleTap,
+              onScaleStart: _onScaleStart,
+              onScaleUpdate: _onScaleUpdate,
+              onTapUp: (details) {
+                final location =
+                    transformer.fromXYCoordsToLatLng(details.localPosition);
 
-                  controller.zoom -= delta.dy / 1000.0;
-                  setState(() {});
-                }
+                final clicked = transformer.fromLatLngToXYCoords(location);
+
+                debugPrint('${location.longitude}, ${location.latitude}');
+                debugPrint('${clicked.dx}, ${clicked.dy}');
+                debugPrint(
+                    '${details.localPosition.dx}, ${details.localPosition.dy}');
               },
-              child: Stack(
-                children: [
-                  Map(
-                    controller: controller,
-                    builder: (context, x, y, z) {
-                      //Google Maps
-                      final darkUrl =
-                          'https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i$z!2i$x!3i$y!4i256!2m3!1e0!2sm!3i556279080!3m17!2sen-US!3sUS!5e18!12m4!1e68!2m2!1sset!2sRoadmap!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2zcC52Om9uLHMuZTpsfHAudjpvZmZ8cC5zOi0xMDAscy5lOmwudC5mfHAuczozNnxwLmM6I2ZmMDAwMDAwfHAubDo0MHxwLnY6b2ZmLHMuZTpsLnQuc3xwLnY6b2ZmfHAuYzojZmYwMDAwMDB8cC5sOjE2LHMuZTpsLml8cC52Om9mZixzLnQ6MXxzLmU6Zy5mfHAuYzojZmYwMDAwMDB8cC5sOjIwLHMudDoxfHMuZTpnLnN8cC5jOiNmZjAwMDAwMHxwLmw6MTd8cC53OjEuMixzLnQ6NXxzLmU6Z3xwLmM6I2ZmMDAwMDAwfHAubDoyMCxzLnQ6NXxzLmU6Zy5mfHAuYzojZmY0ZDYwNTkscy50OjV8cy5lOmcuc3xwLmM6I2ZmNGQ2MDU5LHMudDo4MnxzLmU6Zy5mfHAuYzojZmY0ZDYwNTkscy50OjJ8cy5lOmd8cC5sOjIxLHMudDoyfHMuZTpnLmZ8cC5jOiNmZjRkNjA1OSxzLnQ6MnxzLmU6Zy5zfHAuYzojZmY0ZDYwNTkscy50OjN8cy5lOmd8cC52Om9ufHAuYzojZmY3ZjhkODkscy50OjN8cy5lOmcuZnxwLmM6I2ZmN2Y4ZDg5LHMudDo0OXxzLmU6Zy5mfHAuYzojZmY3ZjhkODl8cC5sOjE3LHMudDo0OXxzLmU6Zy5zfHAuYzojZmY3ZjhkODl8cC5sOjI5fHAudzowLjIscy50OjUwfHMuZTpnfHAuYzojZmYwMDAwMDB8cC5sOjE4LHMudDo1MHxzLmU6Zy5mfHAuYzojZmY3ZjhkODkscy50OjUwfHMuZTpnLnN8cC5jOiNmZjdmOGQ4OSxzLnQ6NTF8cy5lOmd8cC5jOiNmZjAwMDAwMHxwLmw6MTYscy50OjUxfHMuZTpnLmZ8cC5jOiNmZjdmOGQ4OSxzLnQ6NTF8cy5lOmcuc3xwLmM6I2ZmN2Y4ZDg5LHMudDo0fHMuZTpnfHAuYzojZmYwMDAwMDB8cC5sOjE5LHMudDo2fHAuYzojZmYyYjM2Mzh8cC52Om9uLHMudDo2fHMuZTpnfHAuYzojZmYyYjM2Mzh8cC5sOjE3LHMudDo2fHMuZTpnLmZ8cC5jOiNmZjI0MjgyYixzLnQ6NnxzLmU6Zy5zfHAuYzojZmYyNDI4MmIscy50OjZ8cy5lOmx8cC52Om9mZixzLnQ6NnxzLmU6bC50fHAudjpvZmYscy50OjZ8cy5lOmwudC5mfHAudjpvZmYscy50OjZ8cy5lOmwudC5zfHAudjpvZmYscy50OjZ8cy5lOmwuaXxwLnY6b2Zm!4e0&key=AIzaSyAOqYYyBbtXQEtcHG7hwAwyCPQSYidG8yU&token=31440';
-                      return CachedNetworkImage(
-                        imageUrl: darkUrl,
-                        fit: BoxFit.cover,
-                      );
-                    },
-                  ),
-                  Positioned(
-                    top: 35.h,
-                    left: 21.w,
-                    right: 21.w,
-                    child: Container(
-                      height: 66.h,
-                      width: 301.w,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 18.w, vertical: 8.h),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.r),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              SvgPicture.asset(Assets.locationPin),
-                              Text(
-                                  '760 Hattie Ways Suite 680- Montreal, Quebec'),
-                            ],
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 200.w),
-                            child: CustomButton(
-                              onPress: () {},
-                              width: 74.w,
-                              height: 25.h,
-                              title: 'Show',
-                              borderRadius: 12.5.r,
-                              buttonColor: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                        ],
-                      ),
+              child: Listener(
+                behavior: HitTestBehavior.opaque,
+                onPointerSignal: (event) {
+                  if (event is PointerScrollEvent) {
+                    final delta = event.scrollDelta;
+
+                    controller.zoom -= delta.dy / 1000.0;
+                    setState(() {});
+                  }
+                },
+                child: Stack(
+                  children: [
+                    Map(
+                      controller: controller,
+                      builder: (context, x, y, z) {
+                        //Google Maps
+                        final darkUrl =
+                            'https://maps.googleapis.com/maps/vt?pb=!1m5!1m4!1i$z!2i$x!3i$y!4i256!2m3!1e0!2sm!3i556279080!3m17!2sen-US!3sUS!5e18!12m4!1e68!2m2!1sset!2sRoadmap!12m3!1e37!2m1!1ssmartmaps!12m4!1e26!2m2!1sstyles!2zcC52Om9uLHMuZTpsfHAudjpvZmZ8cC5zOi0xMDAscy5lOmwudC5mfHAuczozNnxwLmM6I2ZmMDAwMDAwfHAubDo0MHxwLnY6b2ZmLHMuZTpsLnQuc3xwLnY6b2ZmfHAuYzojZmYwMDAwMDB8cC5sOjE2LHMuZTpsLml8cC52Om9mZixzLnQ6MXxzLmU6Zy5mfHAuYzojZmYwMDAwMDB8cC5sOjIwLHMudDoxfHMuZTpnLnN8cC5jOiNmZjAwMDAwMHxwLmw6MTd8cC53OjEuMixzLnQ6NXxzLmU6Z3xwLmM6I2ZmMDAwMDAwfHAubDoyMCxzLnQ6NXxzLmU6Zy5mfHAuYzojZmY0ZDYwNTkscy50OjV8cy5lOmcuc3xwLmM6I2ZmNGQ2MDU5LHMudDo4MnxzLmU6Zy5mfHAuYzojZmY0ZDYwNTkscy50OjJ8cy5lOmd8cC5sOjIxLHMudDoyfHMuZTpnLmZ8cC5jOiNmZjRkNjA1OSxzLnQ6MnxzLmU6Zy5zfHAuYzojZmY0ZDYwNTkscy50OjN8cy5lOmd8cC52Om9ufHAuYzojZmY3ZjhkODkscy50OjN8cy5lOmcuZnxwLmM6I2ZmN2Y4ZDg5LHMudDo0OXxzLmU6Zy5mfHAuYzojZmY3ZjhkODl8cC5sOjE3LHMudDo0OXxzLmU6Zy5zfHAuYzojZmY3ZjhkODl8cC5sOjI5fHAudzowLjIscy50OjUwfHMuZTpnfHAuYzojZmYwMDAwMDB8cC5sOjE4LHMudDo1MHxzLmU6Zy5mfHAuYzojZmY3ZjhkODkscy50OjUwfHMuZTpnLnN8cC5jOiNmZjdmOGQ4OSxzLnQ6NTF8cy5lOmd8cC5jOiNmZjAwMDAwMHxwLmw6MTYscy50OjUxfHMuZTpnLmZ8cC5jOiNmZjdmOGQ4OSxzLnQ6NTF8cy5lOmcuc3xwLmM6I2ZmN2Y4ZDg5LHMudDo0fHMuZTpnfHAuYzojZmYwMDAwMDB8cC5sOjE5LHMudDo2fHAuYzojZmYyYjM2Mzh8cC52Om9uLHMudDo2fHMuZTpnfHAuYzojZmYyYjM2Mzh8cC5sOjE3LHMudDo2fHMuZTpnLmZ8cC5jOiNmZjI0MjgyYixzLnQ6NnxzLmU6Zy5zfHAuYzojZmYyNDI4MmIscy50OjZ8cy5lOmx8cC52Om9mZixzLnQ6NnxzLmU6bC50fHAudjpvZmYscy50OjZ8cy5lOmwudC5mfHAudjpvZmYscy50OjZ8cy5lOmwudC5zfHAudjpvZmYscy50OjZ8cy5lOmwuaXxwLnY6b2Zm!4e0&key=AIzaSyAOqYYyBbtXQEtcHG7hwAwyCPQSYidG8yU&token=31440';
+                        return CachedNetworkImage(
+                          imageUrl: darkUrl,
+                          fit: BoxFit.cover,
+                        );
+                      },
                     ),
-                  ),
-                  Positioned(
-                    top: 100.h,
-                    right: 152.w,
-                    left: 180.w,
-                    child: ClipPath(
+                    Positioned(
+                      top: 35.h,
+                      left: 21.w,
+                      right: 21.w,
                       child: Container(
-                        height: 20.h,
-                        color: Colors.white,
+                        height: 66.h,
+                        width: 301.w,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 18.w, vertical: 8.h),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8.r),
+                          color: Colors.white,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                SvgPicture.asset(Assets.locationPin),
+                                Text(
+                                    '760 Hattie Ways Suite 680- Montreal, Quebec'),
+                              ],
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(left: 200.w),
+                              child: CustomButton(
+                                onPress: () {},
+                                width: 74.w,
+                                height: 25.h,
+                                title: 'Show',
+                                borderRadius: 12.5.r,
+                                buttonColor: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      clipper: CustomClipPath(),
                     ),
-                  ),
-                  centerMarkerWidget,
-                ],
+                    Positioned(
+                      top: 100.h,
+                      right: 152.w,
+                      left: 180.w,
+                      child: ClipPath(
+                        child: Container(
+                          height: 20.h,
+                          color: Colors.white,
+                        ),
+                        clipper: CustomClipPath(),
+                      ),
+                    ),
+                    centerMarkerWidget,
+                  ],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
